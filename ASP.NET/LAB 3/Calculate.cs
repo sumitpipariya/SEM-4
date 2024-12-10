81895414932c69e458ab1b99604ba4f7ced0e4f0
Sumit Pipariya <146057368+sumitpipariya@users.noreply.github.com>
2024-12-10 18:24:06 +0530

    Add files via upload

---
+++ ASP.NET/LAB 3/Calculate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3
+{
+    interface ICalculate
+    {
+        public void sum(double x, double y);
+        public void substraction(double x, double y);
+    }
+
+    class Calc : ICalculate
+    {
+        public void substraction(double x, double y)
+        {
+            Console.WriteLine("Sutraction is :" + (x - y));
+        }
+
+        public void sum(double x, double y)
+        {
+            Console.WriteLine("Addtion is :" + (x + y));
+        }
+
+    }
+
+}
---
+++ ASP.NET/LAB 3/StringClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3
+{
+    internal class StringClass
+    {
+        public void MethodsOfString()
+        {
+            Console.Write("Enter the First string: ");
+            string a = Console.ReadLine();
+            
+            Console.WriteLine("Length of String is:"+ a.Length);
+            Console.WriteLine("Upper letter of String is:" + a.ToUpper());
+            Console.WriteLine("Lower letter of String is:" + a.ToLower());
+
+            Console.Write("Enter the Second string: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Concate of String is : " + (a +" "+ b));
+        }
+    }
+}
