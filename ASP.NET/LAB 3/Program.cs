a86805494cfdc2f9438b69472be21d3f0ea2ac4e
Sumit Pipariya <146057368+sumitpipariya@users.noreply.github.com>
2024-12-10 13:39:40 +0530

    Add files via upload

---
+++ ASP.NET/LAB 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LAB_3;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the Program number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        switch (n)
+        {
+            case 1:
+                Program1 p = new Program1();
+                p.Divide();
+            break;
+
+            case 2:
+                Arrays a = new Arrays();
+                a.ReadNo();
+            break;
+
+            case 3:
+                Abc ab = new Abc();
+                ab.SumOfTwo(1, 2);
+                ab.SumOfThree(1,2,3);
+            break;
+
+            case 4:
+                Calc calc = new Calc();
+                calc.sum(10, 5);
+                calc.substraction(10,5);
+            break;
+        }
+    }
+}
